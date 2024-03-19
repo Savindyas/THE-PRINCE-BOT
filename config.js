d87f41c4084db68de28b2be10394bdae8100ca77
Savindyas <136538677+Savindyas@users.noreply.github.com>
2024-03-19 19:53:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,14 +12,14 @@ import moment from "moment-timezone"
 
 
 global.owner = [
-  ["94766351670", "Nimsara", false],
+  ["94766351670", "Nimsara", true],
   [''], 
   [''] 
 ]
 //💌------------------------------------------💌
 
 
-//💌global.pairingNumber = "" //put your bot number here💌
+//💌global.pairingNumber = "94779483971" //put your bot number here💌
 global.mods = ['923042205427'] 
 global.prems = ['']
 global.allowed = ['']
@@ -120,10 +120,10 @@ global.developer = 'https://wa.me/message/DCAK67ON3XVOG1' //contact
 
 
 //Sticker WM
-global.botname = '𝘗𝘙𝘐𝘕𝘊𝘌-𝘉𝘖𝘛-𝘔𝘋'
-global.princebot = '🛡️𝘗𝘙𝘐𝘕𝘊𝘌-𝘉𝘖𝘛-𝘔𝘋🛡️'
-global.packname = 'Prince♥️' 
-global.author = 'Prince♥️' 
+global.botname = '𝘕𝘐𝘔SA𝘙A-𝘉𝘖𝘛-𝘙𝘕'
+global.princebot = '🛡️𝘕𝘐𝘔SA𝘙A-𝘉𝘖𝘛-𝘙𝘕🛡️'
+global.packname = '𝘕𝘐𝘔SA𝘙A♥️' 
+global.author = '𝘕𝘐𝘔SA𝘙A♥️' 
 global.princeig = 'https://www.instagram.com' 
 global.princegp = 'https://chat.whatsapp.com/GWJkAJSgbv27sGOMLAzMDS'
 global.menuvid = 'https://i.imgur.com/GFAAXqw.mp4'
